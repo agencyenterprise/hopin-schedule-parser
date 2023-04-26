5ebc781282368c7162018ecf53464ea1b56d477c
Thiago Fassina <thiagoaaa@gmail.com>
2023-04-26 17:32:57 -0300

    parse payload and save file

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 #!/usr/bin/env node
+const fs = require("fs");
+const slugify = require("./slugify");
 
-const eventSlug = process.argv[2];
+const hopinSlug = process.argv[2];
+const webflowSlug = process.argv[3];
 
-fetch(`https://hopin.com/api/v2/schedules/public/${eventSlug}/items`)
-  .then(response => response.json())
-  .then(console.log);
+const header = "Name,Slug,Event,Speakers\n";
+
+const processSchedule = ({ items }) =>
+  items
+    .map((i) => ({
+      name: i.name,
+      slug: [webflowSlug, slugify(i.name)].join("-"),
+      event: webflowSlug,
+      speakers: i.speakers.map((s) => slugify(s.name)).join("; "),
+    }))
+    .reduce(
+      (acc, item) => acc.concat(Object.values(item).join(",")).concat("\n"),
+      header
+    );
+
+const saveFile = (content) => fs.writeFileSync(`${hopinSlug}.csv`, content);
+
+fetch(`https://hopin.com/api/v2/schedules/public/${hopinSlug}/items`)
+  .then((response) => response.json())
+  .then(processSchedule)
+  .then(saveFile);
+
+/*
+
+ASSUMPTIONS
+
+- Hopin endpoint version as of April 2023
+- EventSpeaker slugs are generated automatically by Webflow
+
+*/
